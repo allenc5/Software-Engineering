94ec3a4afdfeefa934dd737cd719188ea90d669c
allenc5 <61200323+allenc5@users.noreply.github.com>
2020-11-11 16:51:18 +0000

    Update test_LCA.java

--- test_LCA.java
+++ test_LCA.java
@@ -38,7 +38,7 @@ public class LCATest {
 		assertEquals("LCA of 4 and 7: ", 1, lca.findLCA(4, 7).data);
 	}
   
-  @Test
+ 	@Test
 	public void testForNonExistentNodes() {
 		// test tree structure:
 		// 1
@@ -57,5 +57,28 @@ public class LCATest {
 		assertEquals("test to find ancestors of non-existent nodes in populated tree: ", null, lca.findLCA(8, 9));
 		assertEquals("test to find ancestors of non-existent nodes in populated tree: ", null, lca.findLCA(23, 100));
 	}
-  
+	
+	@Test
+	public void testSameNodes() {
+		LCA tree = new LCA();
+		tree.root = new Node(8);
+		tree.root.left = new Node(8);
+		tree.root.right = new Node(8);
+		tree.root.left.left = new Node(8);
+		tree.root.left.right = new Node(8);
+		
+		assertEquals("LCA of 8 and 8: ", 8, tree.functionLCA(8, 8));
+	}
+	
+	@Test 
+	public void unevenTree() { 
+		LCA tree = new LCA();
+		tree.root = new Node(1);
+		tree.root.right = new Node(2);
+		tree.root.right.right = new Node(3);
+		
+		assertEquals("LCA of uneven tree:", 1, tree.functionLCA(3, 1));
+		assertEquals("LCA of uneven tree:", 2, tree.functionLCA(2, 3));	
+		
+	}
 }
